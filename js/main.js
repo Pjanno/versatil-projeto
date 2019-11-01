74aa55380cee0dc64d4dc8d7c8bfba4de8559e47
Felipe Ribeiro Chaves <frcchaves@hotmail.com>
2019-11-01 17:11:34 -0300

    Eu deveria ter feito o commit no começo do projeto, anyway, vou só enfeitar com o materialize pra ficar bem apresentável

---
+++ js/main.js
@@ -0,0 +1,66 @@
+var dados = []
+var clientes = []
+var duracaoMedia = []
+var ligacoesOutroDDD = 0
+
+$( document ).ready(()=>{
+  $('#relatorio-btn').prop('disabled', true)
+})
+
+$('#relatorio-btn').click(geraRelatorio)
+
+$('#upload-input').on().change(loadFile)
+
+function loadFile (){
+  console.info('Arquivo carregado')
+  console.log(this.files)
+  processFile(this.files)
+}
+
+function processFile(files){
+  const reader = new FileReader()
+  reader.onload = () => {
+    var lines = reader.result.trim().split('\n')
+    lines = lines.map(line => line.split(';'))
+    header = lines.splice(0, 1)[0]
+    dados = lines
+    console.log({header},{dados})
+  }
+  reader.onloadend = () => {
+    clientes = dados.map(arr => arr[1]) // Retorna array dos números
+    clientes = clientes.filter((elem, i) => clientes.indexOf(elem) === i)// Elimina repetidos
+    $('#relatorio-btn').prop('disabled', false)
+  }
+  reader.readAsText(files[0])
+}
+
+function geraRelatorio(){
+  clientes.forEach(cliente =>{
+    dados.forEach(dado =>{
+      if (cliente == dado[1]){
+        if (cliente.substr(0,2) != dado[2].substr(0,2)){
+          ligacoesOutroDDD++                                    // Mini-Pyramid of Doom
+        }
+      }
+    })
+  })
+
+  var ddd = dados.map(elem => elem[1].substr(0,2))
+      ddd = ddd.filter((elem, i) => ddd.indexOf(elem) === i)
+  
+  ddd.forEach(elem =>{
+    var duracaoMediaArea = {area: null, media: 0}
+        duracaoMediaArea.area = elem
+    var duracao = 0
+    var qnt = 0
+    dados.forEach(dado =>{
+      if (elem == dado[1].substr(0,2)){
+        qnt++
+        duracao += parseInt(dado[3])
+      }
+    })
+    console.log('Duração total: '+duracao+' | Quantidade total: '+qnt)
+    duracaoMediaArea.media = Math.round(duracao / qnt)
+    duracaoMedia.push(duracaoMediaArea)
+  })  
+}
